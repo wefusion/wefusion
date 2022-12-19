1d4723c9d1d0872def32e73794442c81d49ec6de
Stanislav Lyu <wallseat@gmail.com>
2022-12-19 22:34:39 +0300

    Using NLTK dictionaries for prompt split instead regex (#5)

--- stable_diffusion/connection.py
+++ stable_diffusion/connection.py
@@ -1,6 +1,9 @@
+import time
+
 import pika
 import requests
 from pika import BlockingConnection
+from pika.exceptions import AMQPConnectionError
 
 from stable_diffusion.settings import settings
 
@@ -9,11 +12,21 @@ class ConnectionStore:
     _rabbitmq_conn: BlockingConnection
 
     def __init__(self):
+        retry_delay = 5
+        retry_count = 5
+
         pika_conn_params = pika.URLParameters(settings.RABBITMQ_DSN)
-        pika_conn_params._retry_delay = 5
+        pika_conn_params._retry_delay = retry_delay
         pika_conn_params._heartbeat = 120
 
-        self._rabbitmq_conn = pika.BlockingConnection(pika_conn_params)
+        for _ in range(retry_count):
+            try:
+                self._rabbitmq_conn = pika.BlockingConnection(pika_conn_params)
+                break
+            except AMQPConnectionError:
+                time.sleep(retry_delay)
+        else:
+            raise RuntimeError("Failed to connect to RabbitMQ")
 
     def get_channel(self):
         return self._rabbitmq_conn.channel()
